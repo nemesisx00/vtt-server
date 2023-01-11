52d1ec2b2ef489df95f84ac8cc156e2479533cdb
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-01-11 15:36:49 -0500

    Update api.rs
    Removed generateUser() route as a proper /user/create route is being implemented.

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -1,12 +1,11 @@
-#![allow(dead_code, non_snake_case, non_upper_case_globals)]
+#![allow(non_snake_case, non_upper_case_globals)]
+#![cfg_attr(debug_assertions, allow(dead_code))]
 
 use crate::{
 	api::structs::*,
 	database::{
-		findUserById,
-		getDatabaseConnection,
+		findUserById, getDatabaseConnection,
 	},
-	entities::*,
 };
 use actix_web::{
 	get,
@@ -14,45 +13,11 @@ use actix_web::{
 	HttpResponse,
 	Responder,
 };
-use sea_orm::{
-	EntityTrait,
-	Set, ActiveModelTrait,
-};
 
 #[post("/echo")]
 pub async fn echo(reqBody: String) -> impl Responder
 {
 	return HttpResponse::Ok().body(reqBody);
-}
-
-#[get("/generateUser")]
-pub async fn generateUser() -> impl Responder
-{
-	let mut resp = "Failed to create new User.".to_string();
-	if let Ok(db) = getDatabaseConnection().await
-	{
-		if let Ok(Some(_)) = user::Entity::find_by_id(1).one(&db).await
-		{
-			resp = "User already exists!".to_string();
-		}
-		else
-		{
-			let u = user::ActiveModel
-			{
-				label: Set("John Doe".to_owned()),
-				name: Set(Some("Jake Smith".to_owned())),
-				..Default::default()
-			};
-			
-			match u.insert(&db).await
-			{
-				Ok(newUser) => { resp = format!("Created new User with id: {}", newUser.id); },
-				Err(e) => println!("Failed to insert new User: {}", e),
-			}
-		}
-	}
-	
-	return HttpResponse::Ok().body(resp);
 }
 
 #[get("/")]
@@ -64,6 +29,7 @@ pub async fn home() -> impl Responder
 #[post("/login")]
 pub async fn login(json: String) -> impl Responder
 {
+	let db = getDatabaseConnection().await.unwrap();
 	let loginData: LoginData = serde_json::from_str(&json).unwrap();
 	
 	let mut resp = LoginResponseData
@@ -74,7 +40,7 @@ pub async fn login(json: String) -> impl Responder
 	
 	if let Some(userId) = loginData.UserId
 	{
-		if let Some(u) = findUserById(userId).await
+		if let Ok(Some(u)) = findUserById(&db, userId).await
 		{
 			resp.UserId = Some(u.id);
 			resp.Message = format!("Hello {}!", u.label);
