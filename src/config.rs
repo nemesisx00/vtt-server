e0d6f85825f52ff266f317c037f9278a3b60c4e6
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-01-11 15:21:18 -0500

    Refined compiler attributes
    Moved `allow(dead_code)` into a debug-specific attribute across all files, so release builds should report this warning.

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
-#![allow(dead_code, non_snake_case, non_upper_case_globals)]
+#![allow(non_snake_case, non_upper_case_globals)]
+#![cfg_attr(debug_assertions, allow(dead_code))]
 
 use std::{
 	error::Error,
