aedc566f1b5ea12d73bef5ecca3a1a26770133b4
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-01-11 16:47:38 -0500

    Update user.rs
    Updated the userList and userNew routes to retrieve the database connection via actix_web's Data extractor pattern.
    
    This enables the tests to easily set their own database connection, thus leading to fully testable routes.
    
    Updated tests::test_userList and created tests::test_userNew.

--- src/routes/admin/user.rs
+++ src/routes/admin/user.rs
@@ -8,19 +8,20 @@ use crate::{
 	},
 	database::{
 		findAllUsers,
-		createUser, getDatabaseConnection,
+		createUser,
 	},
 };
 use actix_web::{
 	post,
+	web::Data,
 	HttpResponse,
 	Responder,
 };
+use sea_orm::DatabaseConnection;
 
 #[post("/user/list")]
-pub async fn userList(args: String) -> impl Responder
+pub async fn userList(args: String, db: Data<DatabaseConnection>) -> impl Responder
 {
-	let db = getDatabaseConnection().await.unwrap();
 	//println!("POST /admin/user/list: {}", args);
 	//TODO: if validate(args)
 	
@@ -39,24 +40,23 @@ pub async fn userList(args: String) -> impl Responder
 }
 
 #[post("/user/new")]
-pub async fn userNew(args: String) -> impl Responder
+pub async fn userNew(args: String, db: Data<DatabaseConnection>) -> impl Responder
 {
-	let db = getDatabaseConnection().await.unwrap();
 	//TODO: if validate(args)
 	let data = parseArgs(args.to_owned());
 	let mut response = CreateUserResponse {
-		User: None,
-		Message: format!("Invalid data! {}", args),
+		user: None,
+		message: format!("Invalid data! {}", args),
 	};
 	
 	if data.contains_key("username") && !data["username"].is_empty() && data.contains_key("label") && !data["label"].is_empty()
 	{
 		response = match createUser(&db, data["username"].to_owned(), data["label"].to_owned()).await
 		{
-			Ok(user) => CreateUserResponse { User: Some(user), Message: "User created successfully!".to_owned() },
+			Ok(user) => CreateUserResponse { user: Some(user), message: "User created successfully!".to_owned() },
 			Err(e) => {
 				println!("Error creating user: {}", e);
-				CreateUserResponse { User: None, Message: "Failed to create user!".to_owned() }
+				CreateUserResponse { user: None, message: "Failed to create user!".to_owned() }
 			},
 		};
 	}
@@ -69,22 +69,41 @@ pub async fn userNew(args: String) -> impl Responder
 mod tests
 {
 	use crate::{
-		database::createTestDatabase,
+		api::structs::CreateUserResponse,
+		database::{
+			createTestDatabase,
+			createUser,
+			findAllUsers,
+		},
+		entities::User,
 		routes,
 	};
 	use actix_web::{
 		test,
-		web,
+		web::{
+			self,
+			Data,
+		},
 		App,
 	};
+	use serde::Serialize;
+	
+	#[derive(Clone, Debug, Serialize)]
+	struct UserNewData
+	{
+		username: String,
+		label: String,
+	}
 	
 	#[actix_web::test]
-	async fn test_userList_success()
+	async fn test_userList()
 	{
-		let _db = createTestDatabase().await.expect("Failed to create test database!");
+		let db = createTestDatabase().await.unwrap();
+		let user = createUser(&db, "username".to_owned(), "label".to_owned()).await.unwrap();
 		
 		let app = test::init_service(
 			App::new()
+				.app_data(Data::new(db.to_owned()))
 				.service(
 					web::scope("/admin")
 						.service(routes::admin::user::userList)
@@ -92,8 +111,48 @@ mod tests
 		).await;
 		
 		let req = test::TestRequest::post().uri("/admin/user/list").to_request();
-		let resp = test::call_service(&app, req).await;
 		
-		assert!(resp.status().is_success());
+		let resp: Vec<User> = test::call_and_read_body_json(&app, req).await;
+		assert_eq!(resp.len(), 1);
+		assert_eq!(resp[0], user);
+	}
+	
+	#[actix_web::test]
+	async fn test_userNew()
+	{
+		let db = createTestDatabase().await.unwrap();
+		
+		let app = test::init_service(
+			App::new()
+				.app_data(Data::new(db.to_owned()))
+				.service(
+					web::scope("/admin")
+						.service(routes::admin::user::userNew)
+				)
+		).await;
+		
+		let username = "username".to_owned();
+		let label = "label".to_owned();
+		
+		let newData = UserNewData
+		{
+			username: username.to_owned(),
+			label: label.to_owned(),
+		};
+		
+		let req = test::TestRequest::post()
+			.uri("/admin/user/new")
+			.set_form(newData)
+			.to_request();
+		
+		let resp: CreateUserResponse = test::call_and_read_body_json(&app, req).await;
+		let user = resp.user.unwrap();
+		
+		assert_eq!(user.id, 1);
+		assert_eq!(user.username, username);
+		assert_eq!(user.label, label);
+		
+		let users = findAllUsers(&db).await.unwrap();
+		assert_eq!(user, users[0]);
 	}
 }
